24eed6348b552713761dcf66297f6798c4a50b9d
TAKANO Mitsuhiro <takano32@gmail.com>
2015-04-23 03:30:41 +0900

    fix hostname

--- rtkrcvserver/rtkclient.rb
+++ rtkrcvserver/rtkclient.rb
@@ -4,7 +4,7 @@
 
 require 'socket'
 
-rtkrcv_client = TCPSocket.open('localhost', 52001) do |s|
+rtkrcv_client = TCPSocket.open('hl.no32.tk', 52001) do |s|
   while line = s.gets
     puts line.chomp
   end
